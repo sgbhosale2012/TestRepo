10d8cbd0c2f7d4b69aa299425f570c736f12ebf1
unknown <Your Email>
2022-03-09 08:34:09 +0530

    file added

---
+++ pattern.java
@@ -0,0 +1,18 @@
+class pattern {
+
+public static void main(String args[])
+{	int m=10;
+	for(int i=0;i<m;i++)
+
+{	
+for(int j=0;j<m-1;j++)
+{	System.out.print(" ");
+}
+	for(int k=0;k<=i;k++)
+{	
+	System.out.print(" * "+" ");
+}
+System.out.println("");
+
+}
+}}
